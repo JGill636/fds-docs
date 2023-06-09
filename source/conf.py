2e8a3fe9900aa2f26bef14a2eaa3bd4b1c4be55b
Randy McDermott <randy.mcdermott@gmail.com>
2023-06-09 08:28:01 -0400

    conf.py: setting numfig to F seems to fix equation numbering in sphinx 7.1

--- source/conf.py
+++ source/conf.py
@@ -26,8 +26,8 @@ exclude_patterns = []
 # Make sure the target is unique
 autosectionlabel_prefix_document = True
 
-# Enable numref
-numfig = True
+# # Enable numref
+# numfig = True
 
 
 # -- Options for HTML output -------------------------------------------------
